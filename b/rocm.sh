6b702c5b66bf42eeffeb7d932c9a2b272ee7058f
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-03-12 11:19:10 +0100

    stale pada mj ERROR: clGetDeviceIDs(-1)

--- b/rocm.sh
+++ b/rocm.sh
@@ -167,4 +167,4 @@ pip3 install torchvision
 
 cd ~/
 clinfo | grep '  Name:'
-python3 -c "import torch;print('CUDA(hip) is available',torch.cuda.is_available());print('cuda(hip)_device_num:',torch.cuda.device_count());print('Radeon device:',torch.cuda.get_device_name(torch.cuda.current_device()))"
+#python3 -c "import torch;print('CUDA(hip) is available',torch.cuda.is_available());print('cuda(hip)_device_num:',torch.cuda.device_count());print('Radeon device:',torch.cuda.get_device_name(torch.cuda.current_device()))"
