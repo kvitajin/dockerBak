d3fcc9bb5c77ae8311285071130401fada465dfe
Jindřich Kvita <jindrich.kvita@gnj.cz>
2020-03-16 21:00:01 +0100

    neco

--- b/rocm4.sh
+++ b/rocm4.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+cd pytorch-rocm
+
 #python3 tools/amd_build/build_pytorch_amd.py
 #python3 tools/amd_build/build_caffe2_amd.py
 python3 tools/amd_build/build_amd.py
 
-python3 setup.py install
 pip3 install torchvision
+python3 setup.py install
 
